0f131f3b196de8d111abc10980947ad3b3b4583b
Troy Do <hoangdov@gmail.com>
2019-03-22 15:39:09 -0500

    Intro and Advanced R

---
+++ mine.R
@@ -0,0 +1,19 @@
+library(tidytext)
+
+
+case_law_api <- "https://api.case.law/v1/cases/"
+get_cases <- function (question){
+  cases <- c(1, 2, 3)
+  return(cases)
+}
+
+
+# FInd the slander database
+
+# PUll data using http request, crawler
+
+# Parse data into text mining
+
+# Keywords
+
+# response 3 links
