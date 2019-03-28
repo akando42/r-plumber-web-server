b8ad815e764c875ff440be3c87b0e755674b1bcf
Troy Do <hoangdov@gmail.com>
2019-03-28 09:23:01 -0500

    Add Github Commit push

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo -n "Enter your commit message: "
 read ans
 echo $ans
 git commit -am "$ans"
-echo -n "Enter your branch"
+echo -n "Enter your branch: "
 read ans
 echo $ans
 git push origin $ans
