f12ff56a12af08f41daf3bb2509466071c50dea5
Troy Do <hoangdov@gmail.com>
2019-03-29 17:52:30 -0500

    Add packages via scripts

--- init.R
+++ init.R
@@ -1,6 +1,25 @@
+install.packages(c(
+	"plumber", 
+	"httr",
+	"tidyverse", 
+	"tidytext",
+	"rvest",
+	"ggplot2"
+	), 
+	repos = "http://cran.us.r-project.org"
+)
+
 library(plumber)
 r <- plumb("app.R") 
 landing<- PlumberStatic$new("./files/static/")
 r$mount("/",landing)
 r$run(port=7777)
 
+# library(httr)
+# library(tidyverse)
+# library(tidytext)
+# library(wordcloud2)
+# library(R.utils)
+# library(XML)
+# library(stringr)
+# library(ggplot2)
